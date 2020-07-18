7b814dba30c1cdc05dcfc17f758af04a2fdf8d3c
TrungThucHuynh <thuc01629@gmail.com>
2020-07-18 17:28:48 +0700

    add card

---
+++ src/AppStateContext.tsx
@@ -0,0 +1,91 @@
+import React, { createContext, useReducer, useContext } from "react"
+import { findItemIndexById } from './utils/findItemIndexById'
+import { nanoid } from 'nanoid'
+
+
+interface AppStateContextProps {
+	state: AppState
+	dispatch: React.Dispatch<any>
+
+}
+
+const AppStateContext = createContext<AppStateContextProps>({} as AppStateContextProps)
+const appData: AppState = {
+	lists: [
+		{
+			id: "0",
+			text: "To Do",
+			tasks: [{ id: "c0", text: "Generate app scaffold" }]
+		},
+		{
+			id: "1",
+			text: "In Progress",
+			tasks: [{ id: "c2", text: "Learn Typescript" }]
+		},
+		{
+			id: "2",
+			text: "Done",
+			tasks: [{ id: "c3", text: "Begin to use static typing" }]
+		}
+	]
+}
+interface Task {
+	id: string
+	text: string
+}
+interface List {
+	id: string
+	text: string
+	tasks: Task[]
+}
+type Action =
+	| {
+		type: "ADD_LIST"
+		payload: string
+	}
+	| {
+		type: "ADD_TASK"
+		payload: { text: string; taskId: string }
+	}
+export interface AppState {
+	lists: List[]
+}
+
+const appStateReducer = (state: AppState, action: Action): AppState => {
+	switch (action.type) {
+		case "ADD_TASK": {
+			const targetLaneIndex = findItemIndexById(
+				state.lists,
+				action.payload.taskId
+			)
+			state.lists[targetLaneIndex].tasks.push({
+				id: nanoid(),
+				text: action.payload.text
+			})
+			return {
+				...state
+			}
+		}
+		case "ADD_TASK": {
+			const visibilityExample = "Too visible"
+			return {
+				...state
+			}
+		}
+		default: {
+			return state
+		}
+	}
+}
+export const useAppState = () => {
+	return useContext(AppStateContext)
+}
+export const AppStateProvider = ({ children }: React.PropsWithChildren<
+	{}>) => {
+	const [state, dispatch] = useReducer(appStateReducer, appData)
+	return (
+		<AppStateContext.Provider value={{ state, dispatch }}>
+			{children}
+		</AppStateContext.Provider>
+	)
+}
---
+++ src/Card.tsx
@@ -0,0 +1,10 @@
+import React from "react"
+
+import { CardContainer } from "./styles"
+interface CardProps {
+	text: string
+	index: number
+}
+export const Card = ({ text }: CardProps) => {
+	return <CardContainer>{text}</CardContainer>
+}
